1175544865f2b9ffce197aa5865938a362f0f9ad
anacarolinamoreno <carolmoreno@gmail.com>
2022-01-19 14:05:20 -0300

    Coleta de dados - 19/01 - 14h

--- R/script_raspagem-e-analise.R
+++ R/script_raspagem-e-analise.R
@@ -50,10 +50,18 @@ df <- deolhonafila() %>%
     falta_cv = case_when(
       coronavac == 0 ~ "postos_SEM_coronavac",
       coronavac == 1 ~ "postos_COM_coronavac"),
+    falta_jn = case_when(
+      janssen == 0 ~ "postos_SEM_janssen",
+      janssen == 1 ~ "postos_COM_janssen"),
     falta_pf = case_when(
       pfizer == 0 ~ "postos_SEM_pfizer",
-      pfizer == 1 ~ "postos_COM_pfizer"
-    )
+      pfizer == 1 ~ "postos_COM_pfizer"),
+    falta_pf_ped = case_when(
+      pfizer_ped == 0 ~ "postos_SEM_pfizer_pediatrica",
+      pfizer_ped == 1 ~ "postos_COM_pfizer_pediatrica"),
+    falta_flu = case_when(
+      influenza == 0 ~ "postos_SEM_influenza",
+      influenza == 1 ~ "postos_COM_influenza")
   )
 
 # ASTRAZENECA - Calcular porcentagem de postos em cada regiao onde existe falta de Astrazeneca naquele momento
@@ -93,7 +101,7 @@ resumo_az <- bind_rows(
   replace(is.na(.), "TOTAL DA CIDADE")
 
 
-# CORONAVAC - Calcular porcentagem de postos em cada regiao onde existe falta de Astrazeneca naquele momento
+# CORONAVAC - Calcular porcentagem de postos em cada regiao onde existe falta de Coronavac naquele momento
 
 analise_cv <- df %>%
   filter(indice_fila != 5) %>%
@@ -130,7 +138,46 @@ resumo_cv <- bind_rows(
   replace(is.na(.), "TOTAL DA CIDADE")
 
 
-# PFIZER - Calcular porcentagem de postos em cada regiao onde existe falta de Astrazeneca naquele momento
+
+# JANSSEN - Calcular porcentagem de postos em cada regiao onde existe falta de Janssen naquele momento
+
+analise_jn <- df %>%
+  filter(indice_fila != 5) %>%
+  group_by(data_e_hora_atualizacao, regiao_da_cidade, falta_jn) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  pivot_wider(names_from = falta_jn, values_from = contagem) %>%
+  replace(is.na(.), 0) %>%
+  mutate(
+    total_postos_FUNCIONANDO = postos_SEM_janssen + postos_COM_janssen,
+    pct_postos_SEM_janssen = round((( postos_SEM_janssen * 100 ) / total_postos_FUNCIONANDO), 1)
+  )
+
+# JANSSEN - Calcular a mesma porcentagem para a cidade inteira
+
+total_jn <- df %>%
+  filter(indice_fila != 5) %>%
+  group_by(data_e_hora_atualizacao, falta_jn) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  pivot_wider(names_from = falta_jn, values_from = contagem) %>%
+  replace(is.na(.), 0) %>%
+  mutate(
+    total_postos_FUNCIONANDO = postos_SEM_janssen + postos_COM_janssen,
+    pct_postos_SEM_janssen = round((( postos_SEM_janssen * 100 ) / total_postos_FUNCIONANDO), 1)
+  )
+
+# JANSSEN - Juntar as duas tabelas
+
+resumo_jn <- bind_rows(
+  analise_jn, total_jn) %>%
+  replace(is.na(.), "TOTAL DA CIDADE")
+
+
+
+# PFIZER - Calcular porcentagem de postos em cada regiao onde existe falta de Pfizer naquele momento
 
 analise_pf <- df %>%
   filter(indice_fila != 5) %>%
@@ -168,6 +215,79 @@ resumo_pf <- bind_rows(
 
 
 
+# PFIZER PEDIÁTRICA - Calcular porcentagem de postos em cada regiao onde existe falta de Pfizer pediátrica naquele momento
+
+analise_pf_ped <- df %>%
+  filter(indice_fila != 5) %>%
+  group_by(data_e_hora_atualizacao, regiao_da_cidade, falta_pf_ped) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  pivot_wider(names_from = falta_pf_ped, values_from = contagem) %>%
+  replace(is.na(.), 0) %>%
+  mutate(
+    total_postos_FUNCIONANDO = postos_SEM_pfizer_pediatrica + postos_COM_pfizer_pediatrica,
+    pct_postos_SEM_pfizer_pediatrica = round((( postos_SEM_pfizer_pediatrica * 100 ) / total_postos_FUNCIONANDO), 1)
+  )
+
+# PFIZER PEDIÁTRICA - Calcular a mesma porcentagem para a cidade inteira
+
+total_pf_ped <- df %>%
+  filter(indice_fila != 5) %>%
+  group_by(data_e_hora_atualizacao, falta_pf_ped) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  pivot_wider(names_from = falta_pf_ped, values_from = contagem) %>%
+  replace(is.na(.), 0) %>%
+  mutate(
+    total_postos_FUNCIONANDO = postos_SEM_pfizer_pediatrica + postos_COM_pfizer_pediatrica,
+    pct_postos_SEM_pfizer_pediatrica = round((( postos_SEM_pfizer_pediatrica * 100 ) / total_postos_FUNCIONANDO), 1)
+  )
+
+# PFIZER PEDIÁTRICA - Juntar as duas tabelas
+
+resumo_pf_ped <- bind_rows(
+  analise_pf_ped, total_pf_ped) %>%
+  replace(is.na(.), "TOTAL DA CIDADE")
+
+
+# INFLUENZA - Calcular porcentagem de postos em cada regiao onde existe falta de vacina pra Influenza pediátrica naquele momento
+
+analise_flu <- df %>%
+  filter(indice_fila != 5) %>%
+  group_by(data_e_hora_atualizacao, regiao_da_cidade, falta_flu) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  pivot_wider(names_from = falta_flu, values_from = contagem) %>%
+  replace(is.na(.), 0) %>%
+  mutate(
+    total_postos_FUNCIONANDO = postos_SEM_influenza + postos_COM_influenza,
+    pct_postos_SEM_influenza = round((( postos_SEM_influenza * 100 ) / total_postos_FUNCIONANDO), 1)
+  )
+
+# INFLUENZA - Calcular a mesma porcentagem para a cidade inteira
+
+total_flu <- df %>%
+  filter(indice_fila != 5) %>%
+  group_by(data_e_hora_atualizacao, falta_flu) %>%
+  summarise(
+    contagem = sum(contagem)
+  ) %>%
+  pivot_wider(names_from = falta_flu, values_from = contagem) %>%
+  replace(is.na(.), 0) %>%
+  mutate(
+    total_postos_FUNCIONANDO = postos_SEM_influenza + postos_COM_influenza,
+    pct_postos_SEM_influenza = round((( postos_SEM_influenza * 100 ) / total_postos_FUNCIONANDO), 1)
+  )
+
+# INFLUENZA - Juntar as duas tabelas
+
+resumo_flu <- bind_rows(
+  analise_flu, total_flu) %>%
+  replace(is.na(.), "TOTAL DA CIDADE")
+
 # TODAS AS VACINAS - Juntar o resumo das três vacinas
 
 resumo_pct_az <- resumo_az %>%
@@ -176,36 +296,41 @@ resumo_pct_az <- resumo_az %>%
 resumo_pct_cv <- resumo_cv %>%
   select(regiao_da_cidade, pct_postos_SEM_coronavac)
 
+resumo_pct_jn <- resumo_jn %>%
+  select(regiao_da_cidade, pct_postos_SEM_janssen)
+
 resumo_pct_pf <- resumo_pf %>%
   select(regiao_da_cidade, pct_postos_SEM_pfizer)
 
-resumo_mais_atual_az_cv <- left_join(
-  x = resumo_pct_az,
-  y = resumo_pct_cv,
-  by = "regiao_da_cidade"
-) %>%
+resumo_pct_pf_ped <- resumo_pf_ped %>%
+  select(regiao_da_cidade, pct_postos_SEM_pfizer_pediatrica)
+
+resumo_pct_flu <- resumo_flu %>%
+  select(regiao_da_cidade, pct_postos_SEM_influenza)
+
+resumo_mais_atual <- bind_cols(
+  resumo_pct_az,
+  resumo_pct_cv,
+  resumo_pct_jn,
+  resumo_pct_pf,
+  resumo_pct_pf_ped,
+  resumo_pct_flu) %>%
   rename(
-    data_e_hora_atualizacao = data_e_hora_atualizacao.x
+    data_e_hora_atualizacao = data_e_hora_atualizacao...1,
+    regiao_da_cidade = regiao_da_cidade...2
   ) %>%
-  select(data_e_hora_atualizacao,
-         regiao_da_cidade,
-         pct_postos_SEM_astrazeneca,
-         pct_postos_SEM_coronavac)
+  select(
+    data_e_hora_atualizacao,
+    regiao_da_cidade,
+    pct_postos_SEM_astrazeneca,
+    pct_postos_SEM_coronavac,
+    pct_postos_SEM_janssen,
+    pct_postos_SEM_pfizer,
+    pct_postos_SEM_pfizer_pediatrica,
+    pct_postos_SEM_influenza
+  )
 
 
-resumo_mais_atual <- left_join(
-  x = resumo_mais_atual_az_cv,
-  y = resumo_pct_pf,
-  by = "regiao_da_cidade"
-) %>%
-  rename(
-    data_e_hora_atualizacao = data_e_hora_atualizacao.x
-  ) %>%
-  select(data_e_hora_atualizacao,
-         regiao_da_cidade,
-         pct_postos_SEM_astrazeneca,
-         pct_postos_SEM_coronavac,
-         pct_postos_SEM_pfizer)
 
 
 # Criar e salvar arquivo CSV com o resumo
